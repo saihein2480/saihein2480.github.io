36ed1fa8e1d41426593ad23eb5fc0a6bfb132f8d
saihein2480 <u6520051@au.edu>
2024-07-01 09:45:10 +0700

    Edited with JQuery

--- accessory/main.js
+++ accessory/main.js
@@ -1,5 +1,5 @@
 function calculatePrice() {
-    switch (document.getElementById("accessory").value) {
+    switch ($("#accessory").val()) {
         case "CC01":
             return 10;
         case "DC01":
@@ -21,39 +21,34 @@ function calculatePrice() {
 
 function addProductList() {
     // Determine values
-    var accessory_element = document.getElementById("accessory");
-    var accessory_name = accessory_element.options[accessory_element.selectedIndex].text;
-    var accessory_id = document.getElementById("accessory").value;
-    var amount = document.getElementById("amount").value;
+    var accessory_name = $('#accessory option:selected').text();
+    var accessory_id = $("#accessory").val();
+    var amount = $("#amount").val();
     var price = calculatePrice(accessory_id) * amount;
 
-    // Add data to product_list table
-    var table = document.getElementById("product_list");
-    var row = table.insertRow(-1);
-    var accessoryIDCell = row.insertCell(0);
-    var accessoryNameCell = row.insertCell(1)
-    var amountCell = row.insertCell(2);
-    var priceCell = row.insertCell(3);
 
-    // Determine data values to table
-    accessoryIDCell.innerHTML = accessory_id;
-    accessoryNameCell.innerHTML = accessory_name;
-    amountCell.innerHTML = amount;
-    priceCell.innerHTML = "$" + price;
+    // Add data to product_list table
+    const tbodyRef = $('#data tbody')
+    tbodyRef.before(
+        `<tr>
+        <td>${accessory_id}</td>
+        <td>${accessory_name}</td>
+        <td>${amount}</td>
+        <td>${"$" + price}</td>
+        </tr>`)
     updateNetPrice(price);
     calculateVAT();
 }
 var netPrice = 0
 function updateNetPrice(price) {
     var netPriceElement = document.getElementById("netPrice");
-    // var totalPrice = parseFloat(totalPriceElement.innerHTML);
     console.log(netPrice,price)
     netPrice += price;
     netPriceElement.innerHTML = netPrice.toFixed(2);
 }
 function calculateVAT() {
     var totalPriceElement = document.getElementById("totalPrice");
-    var vatRate = document.getElementById("vat").value;
+    var vatRate = $("#vat").val();
     var totalPrice = netPrice * (1 + vatRate / 100);
     totalPriceElement.innerHTML = totalPrice.toFixed(2);
 }
@@ -65,7 +60,7 @@ $(document).ready(function() {
     $("#total_price").click(function(){
         calculateVAT();
     });
-    $("#product_list").css({
+    $("#data").css({
         "background-color":"yellow",
         "border-collapse" : "collapse",
         "width" : "60vw"
